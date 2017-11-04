2f55ff8001ffb3b74e25e2744c4ce80900be4f3c
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2017-11-03 21:51:59 -0500

    safari bug

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 console.log('this works');
 
 $(function () {    
+    $('.nav-icon').on('click touchend', function() { 
+        var link = $(this).attr('href');   
+        window.open(link,'_blank'); // opens in new window as requested 
+
+        return false; // prevent anchor click    
+    });    
+});
+
+$(function () {    
     $('.drop').on('click touchend', function() { 
         var link = $(this).attr('href');   
         location.href = link; // opens in new window as requested 
