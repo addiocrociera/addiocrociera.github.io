ee07a3bd94570a1d6926eea81c11472b012b2266
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2017-11-03 21:27:34 -0500

    touchend

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('this works');
+
+$(function () {    
+    $('.drop').on('click touchend', function() { 
+        var link = $(this).attr('href');   
+        location.href = link; // opens in new window as requested 
+        console.log('what what')
+   
+    });    
+});
