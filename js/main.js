924c96457473d51aa07ddb8c5d8b743cc43988ff
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2017-11-03 22:10:50 -0500

    jquery

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ console.log('this works');
 $(function () {    
     $('.nav-icon').on('click touchend', function() { 
         var link = $(this).attr('href');   
-        window.open(link,'_blank'); // opens in new window as requested 
+        window.open(link); // opens in new window as requested 
 
-        return false; // prevent anchor click    
+           
     });    
 });
 
